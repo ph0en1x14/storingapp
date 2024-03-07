f87af525b2db2672a3d44c7ef9534f8e19e40aeb
ph0en1x14 <phoenix20073101@gmail.com>
2024-03-07 14:52:26 +0100

    Update meldingenController.php

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -5,23 +5,33 @@ $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
 $type = $_POST['soort']
+$overig = $_POST['overige'];
+if (isset($_POST['preoriteit']))
+
 
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-$query="INSERTINTOmeldingen(attractie,type)VALUES(:attractie,:type)";
+$query="INSERT INTO meldingen(attractie, type, melder, capaciteit, prioriteit, overige_info)VALUES(:attractie,:type, :capaciteit, :prioriteit, :overig)";
+
 //3. Prepare
+
 $statement=$conn->prepare($query);
 
 //4. Execute
+
 $statement->execute([
     ":attractie"=>$attractie,
     ":type"=>$type,
-    ":melder"=>$melder
+    ":melder"=>$melder,
+    ":capaciteit"=>$capaciteit,
+    ":prioriteit"=>$prioriteit,
+    ":overig"=>$overig
 ]);
 
 
+$conn=newPDO("mysql:host=$dbHost;dbname=$dbName",$dbUser,$dbPass);
 
 header("Location:../meldingen/index.php?msg=Meldingopgeslagen");
