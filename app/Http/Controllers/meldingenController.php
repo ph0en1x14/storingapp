fc49dcfcd068eb773623de6ec2a4e3edf91f0a03
ph0en1x14 <phoenix20073101@gmail.com>
2024-03-07 14:08:05 +0100

    Update meldingenController.php

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -4,14 +4,24 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
+$type = $_POST['soort']
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-
+$query="INSERTINTOmeldingen(attractie,type)VALUES(:attractie,:type)";
 //3. Prepare
+$statement=$conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie"=>$attractie,
+    ":type"=>$type,
+    ":melder"=>$melder
+]);
+
+
+
+header("Location:../meldingen/index.php?msg=Meldingopgeslagen");
